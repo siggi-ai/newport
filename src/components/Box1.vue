c6b298b7cdb7cea82496e9d7675d44296af429ec
siggi <siegfried.hamm@gmail.com>
2022-03-04 10:50:14 +0100

    rotate -360deg linear

--- src/components/Box1.vue
+++ src/components/Box1.vue
@@ -5,10 +5,22 @@
       rel="stylesheet"
     />
     <div class="box1">
-      <p><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;HELLO<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WORLD</p>
+      <div class="box2">
+        
+      </div>
     </div>
   </div>
 </template>
+<script>
+import Logo from "../assets/logo.png";
+export default {
+  data: function () {
+    return {
+      Logo: Logo,
+    };
+  },
+};
+</script>
 <style scoped>
 .box1 {
   font-family: "Bungee Hairline";
@@ -18,11 +30,31 @@
   background-color: #353535;
   margin-left: 15em;
   margin-top: -6em;
-  border-radius: 0px;
-  transform: rotate(45deg);
+  border-radius: 50%;
+  /* transform: rotate(45deg); */
+  animation: spin 50s linear infinite;
 }
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+
+.box2 {
+    width: 50px;
+  height: 50px;
+  background-color: #4e4e4e;
+}
+
+.vue-logo {
+  margin-top: -10px;
+  margin-left: 25px;
+}
+
 p {
-  margin-top: 5px;
-  line-height: 98%;
+  margin-top: 0px;
 }
 </style>
