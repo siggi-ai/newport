40be91bf0df9dd64014459aa0e7d928cc3bbead9
siggi <siegfried.hamm@gmail.com>
2022-03-16 13:29:12 +0100

    new positions

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,35 @@
       href="https://fonts.googleapis.com/css2?family=Bungee+Hairline&display=swap"
       rel="stylesheet"
     />
-
-    <div>
-      <strong class="name">#Siegfried Hamm</strong>
-      <strong class="title">&#60;WebDeveloper&#62;</strong>
+    <div class="leftbox">
+      <div>
+        <strong class="name">#Siegfried Hamm</strong>
+        <strong class="title">&#60;WebDeveloper&#62;</strong>
+      </div>
     </div>
 
-    <Eyes class="eye1" />
-    <!-- <strong class="description">&#60;CSS Animation&#62;</strong> -->
-    <Eyes class="eye2" />
-    <!-- <div class="nose"></div> -->
-    <br />
-    <!-- <div class="mouth"></div> -->
+    <div class="rightbox">
+      <strong class="description">*websites using:</strong>
+      <br />
+      <img class="three-logos" :src="Logo3" />
+      &nbsp;
+      <img class="vue-logo" :src="Logo" />
+      <br />
+    </div>
 
     <br />
-    <strong class="description">*websites using:</strong>
     <br />
-    <img class="three-logos" :src="Logo3" />
-    &nbsp;
-    <img class="vue-logo" :src="Logo" />
+    <br />
+    <br />
+    <div class="eyes">
+      <Eyes class="eye1" />
+      <!-- <strong class="description">&#60;CSS Animation&#62;</strong> -->
+      <Eyes class="eye2" />
+      <!-- <div class="nose"></div> -->
+      <br />
+      <!-- <div class="mouth"></div> -->
+    </div>
+
     <br />
 
     <ul class="listbox">
@@ -100,6 +110,15 @@ body {
   animation: fade 8s infinite;
 }
 
+.leftbox {
+  display: flex;
+}
+
+.rightbox {
+  margin-left: 288px;
+  margin-top: -100px;
+}
+
 .name {
   font-family: "Bungee Hairline";
   font-size: 20px;
@@ -131,6 +150,10 @@ body {
 .eye2 {
   margin-left: 120px;
   margin-top: -5px;
+}
+
+.eyes {
+  margin-left: 50px;
 }
 
 .nose {
